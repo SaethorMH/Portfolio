11f6d90a9a87928a245b8cb275b35f55f4c02099
SaethorMH <smhspilari@gmail.com>
2026-09-14 17:39:34 +0200

    Add Header, Footer, and MainLayout components; configure global styles and fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Futura",
+      cssVariable: "--futura",
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/FuturaLT.woff2"],
+            weight: "normal",
+            style: "normal",
+          },
+        ],
+      },
+    },
+  ],
+});
