e6d8afd1b0a2b72b0902bf4a851fbba40f19d559
Sæthór Hjálmarsson <91467063+SaethorMH@users.noreply.github.com>
2026-09-15 02:45:26 +0200

    Add site URL to Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig, fontProviders } from "astro/config";
 
 export default defineConfig({
+  site: 'https://smhdesign.dk/',
   fonts: [
     {
       provider: fontProviders.local(),
